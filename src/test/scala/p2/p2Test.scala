d9a540f7e63fec52c66b948eeb2f7d51052026db
kaiwu <kaiwu2004@gmail.com>
2019-05-06 18:49:09 +0800

    add one more test for problem 2

--- src/test/scala/p2/p2Test.scala
+++ src/test/scala/p2/p2Test.scala
@@ -9,8 +9,10 @@ object p2Test extends TestSuite {
       val n1 = listToListNode(List(2, 4, 3))
       val n2 = listToListNode(List(5, 6, 4))
       val n3 = listToListNode(List(7, 0, 8))
+      val n4 = listToListNode(List(2, 7, 2, 1))
       assert(isEqual(n1, n1))
       assert(isEqual(Solution1.addTwoNumbers(n1, n2), n3))
+      assert(isEqual(Solution1.addTwoNumbers(n2, n3), n4))
     }
   }
 }
